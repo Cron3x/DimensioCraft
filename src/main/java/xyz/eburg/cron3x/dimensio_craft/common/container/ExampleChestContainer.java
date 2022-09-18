d3176e3126697a9fecb6ee995101bcfd90038a09
cron3x <craipy.exe@gmail.com>
2022-09-18 19:59:11 +0200

    0x51 - still not working, game crashes while opening example chest (ExampleChestScreen)

--- src/main/java/xyz/eburg/cron3x/dimensio_craft/common/container/ExampleChestContainer.java
+++ src/main/java/xyz/eburg/cron3x/dimensio_craft/common/container/ExampleChestContainer.java
@@ -28,10 +28,8 @@ public class ExampleChestContainer extends AbstractContainerMenu {
 
         final int slotSizePlus2 = 18, startX = 8, startY=159, hotbarY = 217, invY = 18; //slotSizePlus2 = 18, startX = 7, startY=85, hotbarY = 143, invY = 10
 
-
-        for (int column = 0; column < 7; column++) {
-
-            for (int row = 0; row < 9; row++) {
+        for (int row = 0; row < 9; row++) {
+            for (int column = 0; column < 7; column++) {
                 addSlot(new SlotItemHandler(slot, row * 9 + column, startX+column*slotSizePlus2, invY+row*slotSizePlus2));
             }
         }
