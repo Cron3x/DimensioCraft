c4b3ef40abf8c81ca13fec12747601f46761d1ad
cron3x <craipy.exe@gmail.com>
2022-09-18 09:57:35 +0200

    0x51 - cool :)

--- src/main/java/xyz/eburg/cron3x/dimensio_craft/common/container/ExampleChestContainer.java
+++ src/main/java/xyz/eburg/cron3x/dimensio_craft/common/container/ExampleChestContainer.java
@@ -1,18 +1,14 @@
 package xyz.eburg.cron3x.dimensio_craft.common.container;
 
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerData;
-import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.inventory.SimpleContainerData;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import xyz.eburg.cron3x.dimensio_craft.common.blocks.entity.ModBlockEntities;
 
 public class ExampleChestContainer extends AbstractContainerMenu {
     protected ExampleChestContainer(int id, Inventory pInventory) {
