9eec009d4fca22c4673176cf37ca31d8b6d52255
Cron3x <craipy.exe@gmail.com>
2022-09-28 09:53:25 +0200

    begin of multiblock detection v2

--- src/main/java/xyz/eburg/cron3x/dimensio_craft/common/blocks/ElevatorControllerBlock.java
+++ src/main/java/xyz/eburg/cron3x/dimensio_craft/common/blocks/ElevatorControllerBlock.java
@@ -59,13 +59,13 @@ public class ElevatorControllerBlock extends HorizontalDirectionalBlock implemen
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
         if (!level.isClientSide() && level.getBlockEntity(pos) instanceof final ElevatorControllerBlockEntity chest) {
             BlockPos block_top_ns_l0 = pos.offset(0,1,0);
-            BlockPos block_top_ns_cn_l0 = pos.offset(-1,1,0);
-            BlockPos block_top_ns_cp_l0 = pos.offset(1,1,0);
-            BlockPos block_base_ns_cn_l0 = pos.offset(-1,0,0);
-            BlockPos block_base_ns_cp_l0 = pos.offset(1,0,0);
-            BlockPos block_low_ns_l0 = pos.offset(0,-1,0);
-            BlockPos block_low_ns_cn_l0 = pos.offset(-1,-1,0);
-            BlockPos block_low_ns_cp_l0 = pos.offset(1,1,0);
+            BlockPos block_top_ns_cn_l0 = pos.offset(0,1,-1);
+            BlockPos block_top_ns_cp_l0 = pos.offset(0,1,1);
+            BlockPos block_base_ns_cn_l0 = pos.offset(0,0,-1);
+            BlockPos block_base_ns_cp_l0 = pos.offset(0,0,1);
+            BlockPos block_low_ns_l0 = pos.offset(0,-1,-1);
+            BlockPos block_low_ns_cn_l0 = pos.offset(0,-1,1);
+            BlockPos block_low_ns_cp_l0 = pos.offset(0,1,1);
 
             BlockPos block_top_ew_l0 = pos.offset(0,1,0);
             BlockPos block_top_ew_cn_l0 = pos.offset(-1,1,0);
@@ -77,49 +77,53 @@ public class ElevatorControllerBlock extends HorizontalDirectionalBlock implemen
             BlockPos block_low_ew_cp_l0 = pos.offset(1,1,0);
 
 
-            BlockPos block_top_ns_l1 = pos.offset(0,1,0);
-            BlockPos block_top_ns_cn_l1 = pos.offset(-1,1,0);
-            BlockPos block_top_ns_cp_l1 = pos.offset(1,1,0);
-            BlockPos block_base_ns_cn_l1 = pos.offset(-1,0,0);
-            BlockPos block_base_ns_cp_l1 = pos.offset(1,0,0);
-            BlockPos block_low_ns_l1 = pos.offset(0,-1,0);
-            BlockPos block_low_ns_cn_l1 = pos.offset(-1,-1,0);
-            BlockPos block_low_ns_cp_l1 = pos.offset(1,1,0);
+            BlockPos block_top_ns_l1 = pos.offset(-1,1,0);
+            BlockPos block_top_ns_cn_l1 = pos.offset(-1,1,-1);
+            BlockPos block_top_ns_cp_l1 = pos.offset(-1,1,1);
+            BlockPos block_base_ns_l1 = pos.offset(-1,0,-1);
+            BlockPos block_base_ns_cn_l1 = pos.offset(-1,0,-1);
+            BlockPos block_base_ns_cp_l1 = pos.offset(-1,0,1);
+            BlockPos block_low_ns_l1 = pos.offset(-1,-1,-1);
+            BlockPos block_low_ns_cn_l1 = pos.offset(-1,-1,1);
+            BlockPos block_low_ns_cp_l1 = pos.offset(-1,1,-1);
 
-            BlockPos block_top_ew_l1 = pos.offset(0,1,0);
-            BlockPos block_top_ew_cn_l1 = pos.offset(-1,1,0);
-            BlockPos block_top_ew_cp_l1 = pos.offset(1,1,0);
-            BlockPos block_base_ew_cn_l1 = pos.offset(-1,0,0);
-            BlockPos block_base_ew_cp_l1 = pos.offset(1,0,0);
-            BlockPos block_low_ew_l1 = pos.offset(0,-1,0);
-            BlockPos block_low_ew_cn_l1 = pos.offset(-1,-1,0);
-            BlockPos block_low_ew_cp_l1 = pos.offset(1,1,0);
+            BlockPos block_top_ew_l1 = pos.offset(0,1,-1);
+            BlockPos block_top_ew_cn_l1 = pos.offset(-1,1,-1);
+            BlockPos block_top_ew_cp_l1 = pos.offset(1,1,-1);
+            BlockPos block_base_ew_l1 = pos.offset(0,0,-1);
+            BlockPos block_base_ew_cn_l1 = pos.offset(-1,0,-1);
+            BlockPos block_base_ew_cp_l1 = pos.offset(1,0,-1);
+            BlockPos block_low_ew_l1 = pos.offset(0,-1,-1);
+            BlockPos block_low_ew_cn_l1 = pos.offset(-1,-1,-1);
+            BlockPos block_low_ew_cp_l1 = pos.offset(1,1,-1);
 
 
-            BlockPos block_top_ns_l2 = pos.offset(0,1,0);
-            BlockPos block_top_ns_cn_l2 = pos.offset(-1,1,0);
-            BlockPos block_top_ns_cp_l2 = pos.offset(1,1,0);
-            BlockPos block_base_ns_cn_l2 = pos.offset(-1,0,0);
-            BlockPos block_base_ns_cp_l2 = pos.offset(1,0,0);
-            BlockPos block_low_ns_l2 = pos.offset(0,-1,0);
-            BlockPos block_low_ns_cn_l2 = pos.offset(-1,-1,0);
-            BlockPos block_low_ns_cp_l2 = pos.offset(1,1,0);
+            BlockPos block_top_ns_l2 = pos.offset(-2,1,0);
+            BlockPos block_top_ns_cn_l2 = pos.offset(-2,1,-1);
+            BlockPos block_top_ns_cp_l2 = pos.offset(-2,1,1);
+            BlockPos block_base_ns_l2 = pos.offset(-2,0,0);
+            BlockPos block_base_ns_cn_l2 = pos.offset(-2,0,-1);
+            BlockPos block_base_ns_cp_l2 = pos.offset(-2,0,1);
+            BlockPos block_low_ns_l2 = pos.offset(-2,-1,0);
+            BlockPos block_low_ns_cn_l2 = pos.offset(-2,-1,-1);
+            BlockPos block_low_ns_cp_l2 = pos.offset(-2,1,1);
 
-            BlockPos block_top_ew_l2 = pos.offset(0,1,0);
-            BlockPos block_top_ew_cn_l2 = pos.offset(-1,1,0);
-            BlockPos block_top_ew_cp_l2 = pos.offset(1,1,0);
-            BlockPos block_base_ew_cn_l2 = pos.offset(-1,0,0);
-            BlockPos block_base_ew_cp_l2 = pos.offset(1,0,0);
-            BlockPos block_low_ew_l2 = pos.offset(0,-1,0);
-            BlockPos block_low_ew_cn_l2 = pos.offset(-1,-1,0);
-            BlockPos block_low_ew_cp_l2 = pos.offset(1,1,0);
+            BlockPos block_top_ew_l2 = pos.offset(0,1,-2);
+            BlockPos block_top_ew_cn_l2 = pos.offset(-1,1,-2);
+            BlockPos block_top_ew_cp_l2 = pos.offset(1,1,-2);
+            BlockPos block_base_ew_l2 = pos.offset(0,0,-2);
+            BlockPos block_base_ew_cn_l2 = pos.offset(-1,0,-2);
+            BlockPos block_base_ew_cp_l2 = pos.offset(1,0,-2);
+            BlockPos block_low_ew_l2 = pos.offset(0,-1,-2);
+            BlockPos block_low_ew_cn_l2 = pos.offset(-1,-1,-2);
+            BlockPos block_low_ew_cp_l2 = pos.offset(1,1,-2);
             //player.sendMessage(new TextComponent(":> (" + block_n10n1.getX()+" "+block_n10n1.getY()+" "+block_n10n1.getZ() + "): " + level.getBlockState(block_n10n1)), player.getUUID()); ;
 
             if (       level.getBlockState(block_low_ew_l0).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
                     && level.getBlockState(block_top_ns_l0).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
             ){
                 player.sendMessage(new TextComponent("block_low_ew + block_top_ns"), player.getUUID());
-                if(      // Layer 1
+                if      // Layer 1
                     (       level.getBlockState(block_top_ns_cn_l0).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
                          && level.getBlockState(block_top_ns_cp_l0).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
                          && level.getBlockState(block_base_ns_cp_l0).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
@@ -127,7 +131,22 @@ public class ElevatorControllerBlock extends HorizontalDirectionalBlock implemen
                          && level.getBlockState(block_low_ns_l0).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
                          && level.getBlockState(block_low_ns_cn_l0).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
                          && level.getBlockState(block_low_ns_cp_l0).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
-                    ) || (
+                    ) {
+                        if      // Layer 1
+                        (       level.getBlockState(block_top_ns_cn_l1).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
+                                && level.getBlockState(block_top_ns_cp_l1).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
+                                && level.getBlockState(block_base_ns_cp_l1).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
+                                && level.getBlockState(block_base_ns_cn_l1).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
+                                && level.getBlockState(block_low_ns_l1).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
+                                && level.getBlockState(block_low_ns_cn_l1).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
+                                && level.getBlockState(block_low_ns_cp_l1).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
+                                && level.getBlockState(block_low_ns_l1).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
+                                && level.getBlockState(block_base_ns_l1).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
+                                && level.getBlockState(block_top_ns_l1).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
+                        ) {
+
+                        }
+                } else if (
                             level.getBlockState(block_top_ew_l0).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
                          && level.getBlockState(block_base_ew_cn_l0).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
                          && level.getBlockState(block_base_ew_cp_l0).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
@@ -136,48 +155,9 @@ public class ElevatorControllerBlock extends HorizontalDirectionalBlock implemen
                          && level.getBlockState(block_low_ew_cn_l0).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
                          && level.getBlockState(block_low_ew_cp_l0).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
                     )
-                ) {
-                    player.sendMessage(new TextComponent("layer0"), player.getUUID());
-                    if(          // Layer 2 //TODO: Add middle block Variable and correct valid blocks
-                            (       level.getBlockState(block_top_ns_cn_l1).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
-                                 && level.getBlockState(block_top_ns_cp_l1).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
-                                 && level.getBlockState(block_base_ns_cp_l1).getBlock().equals()
-                                 && level.getBlockState(block_base_ns_cn_l1).getBlock().equals()
-                                 && level.getBlockState(block_low_ns_l1).getBlock().equals()
-                                 && level.getBlockState(block_low_ns_cn_l1).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
-                                 && level.getBlockState(block_low_ns_cp_l1).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
-                            ) || (
-                                    level.getBlockState(block_top_ew_l0).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
-                                 && level.getBlockState(block_base_ew_cn_l1).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
-                                 && level.getBlockState(block_base_ew_cp_l1).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
-                                 && level.getBlockState(block_top_ew_cn_l1).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
-                                 && level.getBlockState(block_top_ew_cp_l1).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
-                                 && level.getBlockState(block_low_ew_cn_l1).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
-                                 && level.getBlockState(block_low_ew_cp_l1).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
-                            )
-                    ) {
-                        player.sendMessage(new TextComponent("layer1"), player.getUUID());
-                        if(          // Layer 3
-                                (       level.getBlockState(block_top_ns_cn_l2).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
-                                     && level.getBlockState(block_top_ns_cp_l2).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
-                                     && level.getBlockState(block_base_ns_cp_l2).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
-                                     && level.getBlockState(block_base_ns_cn_l2).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
-                                     && level.getBlockState(block_low_ns_l2).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
-                                     && level.getBlockState(block_low_ns_cn_l2).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
-                                     && level.getBlockState(block_low_ns_cp_l2).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
-                                ) || (
-                                        level.getBlockState(block_top_ew_l2).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
-                                     && level.getBlockState(block_base_ew_cn_l2).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
-                                     && level.getBlockState(block_base_ew_cp_l2).getBlock().equals(ModBlocks.STRUCTURE_FRAME_IRON_BLOCK.get())
-                                     && level.getBlockState(block_top_ew_cn_l2).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
-                                     && level.getBlockState(block_top_ew_cp_l2).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
-                                     && level.getBlockState(block_low_ew_cn_l2).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
-                                     && level.getBlockState(block_low_ew_cp_l2).getBlock().equals(Blocks.LIGHT_GRAY_CONCRETE)
-                                )
-                        ) {
-                            player.sendMessage(new TextComponent("layer2"), player.getUUID());
-                        }
-                    }
+                {
+
+                } else {
                 }
             }
 
